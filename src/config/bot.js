2b66f92d7c552a642d8f55a11a5f0ba15fdfd6d4
itzdark3n3d <egemhalil3@gmail.com>
2026-05-13 23:59:36 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Created and mainted exclusively by _dark3n3d_, meant for the thorns smp.",
         // Activity type number (0 = Playing).
         type: 0, 
       },
